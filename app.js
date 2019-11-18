7c91800b2f4f0022180cfdd5505aaa2361ebc6b2
Kenny Stevens <Kenny@Kennys-MacBook-Pro.local>
2019-11-18 10:16:15 +0000

    add portfolio contents

---
+++ app.js
@@ -0,0 +1,12 @@
+const express 		= require("express"),
+	  app 			= express(),
+	  bodyParser 	= require("body-parser")
+	  
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs")
+
+app.get("/", function(req, res){
+	res.render("landing")
+});
+
+app.listen(3000)
