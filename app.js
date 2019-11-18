71c0fc8739d7016bef9e888c636fa71bdc9a67a7
Kenny Stevens <Kenny@Kennys-MacBook-Pro.local>
2019-11-18 11:48:04 +0000

    add http-server module

--- app.js
+++ app.js
@@ -9,4 +9,8 @@ app.get("/", function(req, res){
 	res.render("landing")
 });
 
-app.listen(3000)
+app.listen(3000, function(){
+	console.log("=======================")
+	console.log("     Server is live")
+	console.log("=======================")
+})
